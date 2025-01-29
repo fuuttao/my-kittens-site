0d9be94347899f1a01d87d502868578af26ae9b5
fuuttao <umarbaevanigora93@gmail.com>
2025-01-29 12:17:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+// Пример JavaScript, чтобы показывать сообщение при нажатии на кнопку
+document.addEventListener('DOMContentLoaded', function () {
+    const messageButton = document.createElement('button');
+    messageButton.textContent = 'Нажми на меня!';
+    document.body.appendChild(messageButton);
+
+    messageButton.addEventListener('click', function () {
+        alert('С любовью, твоя манюня!');
+    });
+});
